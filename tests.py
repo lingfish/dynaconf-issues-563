13b73b3cca00371b83312b730ed0515c7ff2715b
Jason Lingohr <lingfish@users.noreply.github.com>
2024-02-23 10:23:10 +1100

    Squash.

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
 import pytest
-from mypkg.config import settings
+from config import settings
 
 @pytest.fixture(scope="session", autouse=True)
 def set_test_settings():
     """Forces usage of [testing] env from `settings.toml` for tests"""
     settings.configure(FORCE_ENV_FOR_DYNACONF="testing")
   
+def test_running_on_testing_environment():
+    assert settings.current_env == "testing"
+    assert settings.ENV_FOR_DYNACONF == "testing"
+    
