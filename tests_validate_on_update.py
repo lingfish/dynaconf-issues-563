02544929312585e7bc866ecd6bc73b94d8f519bf
Jason Lingohr <lingfish@users.noreply.github.com>
2024-02-23 12:20:40 +1100

    Create tests_validate_on_update.py

---
+++ tests_validate_on_update.py
@@ -0,0 +1,12 @@
+import pytest
+from config import settings
+
+@pytest.fixture(scope="session", autouse=True)
+def set_test_settings():
+    """Forces usage of [testing] env from `settings.toml` for tests"""
+    settings.configure(FORCE_ENV_FOR_DYNACONF="testing", VALIDATE_ON_UPDATE_FOR_DYNACONF=True)
+  
+def test_running_on_testing_environment():
+    assert settings.current_env == "testing"
+    assert settings.ENV_FOR_DYNACONF == "testing"
+    
